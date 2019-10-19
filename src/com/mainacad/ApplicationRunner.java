a0e196d6cc82d2603153c17e517b195bad3c9b37
AlexK-Com <alexk.com@gmail.com>
2019-10-19 23:03:03 +0300

    Vasche Vsio!

--- src/com/mainacad/ApplicationRunner.java
+++ src/com/mainacad/ApplicationRunner.java
@@ -11,9 +11,9 @@ public class ApplicationRunner {
         long  multiply1 = 0;
         long  multiply2 = 0;
         Scanner scanner = new Scanner(System.in);
-        LOGGER.info("Input Numbers 1: ");
+        LOGGER.info("Input Number 1: ");
         multiply1 = Long.parseLong(scanner.nextLine()); // считывает данные с клавиатуры
-        LOGGER.info("Input Numbers 2: ");
+        LOGGER.info("Input Number 2: ");
         multiply2 = Long.parseLong(scanner.nextLine()); // считывает данные с клавиатуры
 
         Multiply multiply = new Multiply();
